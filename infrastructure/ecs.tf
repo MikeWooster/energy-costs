f7033c559b6cdf0512bd6a1b1828bcc4875fc830
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-12-04 15:01:19 +0000

    spinning up fargate service

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -11,14 +11,14 @@ data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
 }
 
 resource "aws_iam_role" "task_role" {
-  name = "mikes-ecs-task-role"
-  description = "Role used as the Task Role in the ECS cluster"
+  name               = "mikes-ecs-task-role"
+  description        = "Role used as the Task Role in the ECS cluster"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
 }
 
 resource "aws_iam_role" "execution_role" {
-  name = "mikes-ecs-execution-role"
-  description = "Role used as the Execution Role in the ECS cluster"
+  name               = "mikes-ecs-execution-role"
+  description        = "Role used as the Execution Role in the ECS cluster"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
 }
 
@@ -28,14 +28,15 @@ resource "aws_ecs_task_definition" "webservers" {
   family                = "mikes-webservers"
   container_definitions = file("task-definitions/webservers.json")
 
-  task_role_arn = aws_iam_role.task_role.arn
+  task_role_arn      = aws_iam_role.task_role.arn
   execution_role_arn = aws_iam_role.execution_role.arn
+
+  requires_compatibilities = ["FARGATE"]
 
   # Fargate requires awsvpc
   network_mode = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
 
-  cpu =   "256"
+  cpu    = "256"
   memory = "512"
 }
 
@@ -43,16 +44,23 @@ resource "aws_ecs_cluster" "main" {
   name = "mikes-ecs-cluster"
 }
 
-# resource "aws_ecs_service" "webservers" {
-#   name            = "webservers"
-#   cluster         = aws_ecs_cluster.main.id
-#   task_definition = aws_ecs_task_definition.webservers.arn
-#   desired_count   = 1
-#   iam_role        = aws_iam_role.foo.arn
-#   depends_on      = [aws_iam_role_policy.foo]
+resource "aws_ecs_service" "webservers" {
+  depends_on = [
+    aws_ecs_cluster.main,
+    aws_ecs_task_definition.webservers,
+    aws_subnet.public_az1,
+    aws_subnet.public_az2,
+    aws_subnet.public_az3,
+  ]
 
-#   ordered_placement_strategy {
-#     type  = "spread"
-#     field = "attribute:ecs.availability-zone"
-#   }
-# }
+  name            = "mikes-webservers"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.webservers.arn
+  launch_type     = "FARGATE"
+  desired_count   = 0
+
+  network_configuration {
+    subnets          = [aws_subnet.public_az1.id, aws_subnet.public_az2.id, aws_subnet.public_az3.id]
+    assign_public_ip = true
+  }
+}
