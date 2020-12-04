e884614abf0971192f87e8fa00be5ed3e5549967
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-12-04 17:18:36 +0000

    service is working - cant see anything though

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -22,11 +22,27 @@ resource "aws_iam_role" "execution_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
 }
 
-resource "aws_ecs_task_definition" "webservers" {
-  depends_on = [aws_iam_role.task_role, aws_iam_role.execution_role]
+data "aws_iam_policy" "AmazonECSTaskExecutionRolePolicy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
-  family                = "mikes-webservers"
-  container_definitions = file("task-definitions/webservers.json")
+resource "aws_iam_role_policy_attachment" "execution_role_aws_task_execution_role_policy" {
+  role       = aws_iam_role.execution_role.name
+  policy_arn = data.aws_iam_policy.AmazonECSTaskExecutionRolePolicy.arn
+}
+
+resource "aws_ecs_task_definition" "webservers" {
+  depends_on = [
+    aws_iam_role.task_role,
+    aws_iam_role.execution_role,
+    aws_ecr_repository.main,
+  ]
+
+  family = "mikes-webservers"
+  container_definitions = templatefile("task-definitions/webservers.json", {
+    image_url = aws_ecr_repository.main.repository_url,
+    image_tag = "latest",
+  })
 
   task_role_arn      = aws_iam_role.task_role.arn
   execution_role_arn = aws_iam_role.execution_role.arn
@@ -48,6 +64,7 @@ resource "aws_ecs_service" "webservers" {
   depends_on = [
     aws_ecs_cluster.main,
     aws_ecs_task_definition.webservers,
+    aws_security_group.web_traffic,
     aws_subnet.public_az1,
     aws_subnet.public_az2,
     aws_subnet.public_az3,
@@ -57,10 +74,11 @@ resource "aws_ecs_service" "webservers" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.webservers.arn
   launch_type     = "FARGATE"
-  desired_count   = 0
+  desired_count   = 1
 
   network_configuration {
     subnets          = [aws_subnet.public_az1.id, aws_subnet.public_az2.id, aws_subnet.public_az3.id]
     assign_public_ip = true
+    security_groups  = [aws_security_group.web_traffic.id]
   }
 }
